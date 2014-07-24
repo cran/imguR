b4e3cbf0085e44081bccdd14e42d8bb7100ff3c1
Thomas J. Leeper <thosjleeper@gmail.com>
2014-07-24 14:16:29 +0000

    version 1.0.0

--- R/imguR.R
+++ R/imguR.R
@@ -1,15 +1,43 @@
-imguR <- function(title=NULL, caption=NULL, name=NULL, ...) {
-    tmpfile <- tempfile()
-    pdf(tmpfile, ...)
-    which <- dev.cur()
-    if (!exists(".imguR.devices", envir=.GlobalEnv)) imguR.dev <- list()
-        else imguR.dev <- get(".imguR.devices", envir=.GlobalEnv)
-    imguR.dev[[which]] <- list(filename=tmpfile, title=title, caption=caption, name=name)
-    assign(".imguR.devices", imguR.dev, envir=.GlobalEnv)
-    invisible()
+imgur <-
+imguR <- 
+function(device = png,
+         file = NULL,
+         title = NULL, 
+         description = NULL, 
+         album = NULL,
+         name = NULL, 
+         key = NULL,
+         token = NULL,
+         ...) {
+    if(!is.null(file)) {
+        tmpfile <- file
+        delete <- FALSE
+    } else {
+        tmpfile <- tempfile()
+        delete <- TRUE
+    }
+    do.call(device, c(file = tmpfile, list(...)))
+    out <- list(file = tmpfile,
+                current = dev.cur(),
+                title = title, 
+                description = description, 
+                name = name,
+                delete = delete)
+    if(!is.null(key))
+        out$key <- key
+    if(!is.null(token))
+        out$token <- token
+    structure(out, class = 'imgur_device')
 }
 
-to.imguR <- function() {
-    dev.copy(imguR)
-    dev.off()
+imgur_off <-
+function(obj, ...) {
+    if(!inherits(obj, 'imgur_device'))
+        stop("'obj' is not of class 'imgur_device'")
+    if(obj$current %in% dev.list())
+        dev.off(obj$current)
+    tmp <- do.call(upload_image, c(obj, list(...)))
+    if(obj$delete)
+        unlink(obj$file)
+    return(tmp)
 }
